fa000c7419d2613651de0e45f2c93005271723c3
afnan-emumba <afnan.sohail@emumba.com>
2025-01-10 13:15:22 +0500

    theme setup

---
+++ src/theme/theme.ts
@@ -0,0 +1,73 @@
+export const antTheme = {
+  token: {
+    colorPrimary: "#003B44",
+    colorInfo: "#7539ff",
+    colorSuccess: "#08b461",
+    colorWarning: "#f7bb43",
+    colorError: "#E30000",
+    borderRadius: 4,
+  },
+  components: {
+    Button: {
+      paddingBlock: 8,
+      paddingInline: 32,
+    },
+  },
+};
+
+// export const antTheme = {
+//   token: {
+//     colorPrimary: "#7539ff",
+//     colorInfo: "#7539ff",
+//     colorSuccess: "#08b461",
+//     colorWarning: "#f7bb43",
+//     colorError: "#ef4435",
+//     borderRadius: 4,
+//   },
+//   components: {
+//     Button: {
+//       paddingBlock: 8,
+//       paddingInline: 16,
+//       colorBgDisabled: "red",
+//       colorTextDisabled: "white",
+//       borderColorDisabled: "transparent",
+//       colorBgContainerDisabled: "#B6B6B6",
+//     },
+//     Input: {
+//       paddingBlock: 4,
+//       paddingInline: 12,
+//       colorBgContainer: "#EFF4FB",
+//       border: "1px solid #DDE4F0",
+//       color: "#333",
+//     },
+//     Table: {
+//       borderColor: "#DDE4F0",
+//       cellPaddingBlock: 14,
+//       headerBg: "none",
+//       headerSplitColor: "none",
+//       rowHoverBg: "#FAFAFA",
+//       headerBorderRadius: 8,
+//     },
+//     Pagination: {
+//       itemActiveBg: "#7539ff",
+//       colorPrimary: "white",
+//       itemSize: 28,
+//       colorText: "#667085",
+//     },
+//     Card: {
+//       headerBg: "#F7F7F7",
+//       borderRadiusLG: 8,
+//     },
+//     Modal: {
+//       titleFontSize: 20,
+//     },
+//     Alert: {
+//       colorSuccess: "#08B461",
+//       colorSuccessBg: "#E1F3EC",
+//       colorSuccessBorder: "#3EC484",
+//       colorError: "#EA3B3B",
+//       colorErrorBg: "#F3E1EA",
+//       colorErrorBorder: "#C43E5E",
+//     },
+//   },
+// };
